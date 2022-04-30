d88a635bbabb2b848f6897a8c41ccd5bed4c7503
Yash24031991 <104666043+Yash24031991@users.noreply.github.com>
2022-04-29 23:50:24 -0500

    Update streamlit_app.py
    Added text and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Moms new healthy Dinner')
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 and Blueberry Oat meal');
+streamlit.text('Masala Dosa');
+streamlit.text('Berry Milkshake');
+streamlit.text('Bread and Jam');
