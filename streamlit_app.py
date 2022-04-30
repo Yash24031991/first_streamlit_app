412515948b103130816d481a46eedd7211ec02e7
Yash24031991 <104666043+Yash24031991@users.noreply.github.com>
2022-04-30 15:58:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
-streamlit.title('My Moms New Healthy Dinner')
-streamlit.header('Breakfast Favorites');
-streamlit.text('Omega 3 and Blueberry Oat meal');
-streamlit.text('Masala Dosa');
-streamlit.text('Berry Milkshake');
-streamlit.text('Bread and Jam');
-streamlit.markdown(":bowtie:");
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
