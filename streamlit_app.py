9585a002da11abed72488825053a1efde22235ec
Yash24031991 <104666043+Yash24031991@users.noreply.github.com>
2022-05-01 23:08:24 -0500

    Update streamlit_app.py
    editing the error

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Pick the fruit which they want
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-#display table on the page
-streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice .com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+#display table on the page
+streamlit.dataframe(fruits_to_show)
 
 
 
