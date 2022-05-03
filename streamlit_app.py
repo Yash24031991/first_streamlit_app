dda9d76fbcf4b8368871f7f42dbd1edea5c38f88
Yash24031991 <104666043+Yash24031991@users.noreply.github.com>
2022-05-02 19:16:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import pandas
 import streamlit
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
